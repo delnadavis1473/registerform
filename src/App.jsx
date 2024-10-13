d5f8b0b11104e05e6f3c15657428836613f1a9b6
delnadavis1473 <delnaddavis1473@gmail.com>
2024-10-13 20:56:40 +0530

    regform v1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudentForm from './components/StudentRegisterForm';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentForm />
+    </div>
+  );
+}
+
+export default App;
